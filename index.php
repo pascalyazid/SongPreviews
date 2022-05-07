31003cf3d83b1f5fb768d45aca556af09569d6a9
pascalyazid <pascal.thuma@gmail.com>
2022-05-07 19:19:53 +0200

    Title updated

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.8, maximum-scale=1, user-scalable=no">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <title>Deezer Search Artist</title>
+    <title>Song Previews</title>
     <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="script.js" charset="utf-8"></script>
@@ -37,7 +37,7 @@
           <div class="row">
             <div class="col"></div>
             <div class="col d-flex justify-content-center">
-              <h1>Search Preview</h1>
+              <h1>Song Previews</h1>
             </div>
             <div class="col"></div>
           </div>
