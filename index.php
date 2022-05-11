990cb43ee262256b3316eb5f8f033778a9973dfa
pascalyazid <pascal.thuma@gmail.com>
2022-05-11 09:28:57 +0200

    Mobile view updated

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=0.8, maximum-scale=1, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=0.75, maximum-scale=1, user-scalable=no">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>Song Previews</title>
     <!-- JavaScript Bundle with Popper -->
