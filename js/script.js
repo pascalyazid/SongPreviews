4a97a95e8a51bdd0d659cf430f16042abf2c099c
Pascal Thuma <62385237+pascalyazid@users.noreply.github.com>
2022-09-07 18:36:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(document).ready(function() {
 });
 
@@ -50,56 +50,3 @@ function initPlayerTest() {
   }
 });
 }
-=======
-$(document).ready(function() {
-});
-
-function loadNext() {
-  $.get("next.php", function(data){
-    document.body.innerHTML = (document.body.innerHTML + data);
-  });
-}
-
-function setSRC(url) {
-  let audio = document.getElementById('audio');
-  audio.volume = 0.3;
-  let button = document.getElementById(url);
-
-  if(button.innerHTML == "Play") {
-    if(audio.src != "") {
-      let old = document.getElementById(audio.src);
-      if (old.innerHTML = "Pause") {
-        audio.pause;
-        old.innerHTML = "Play";
-      }
-    }
-
-    audio.src = url;
-    audio.play();
-    button.innerHTML = "Pause";
-  }
-
-
-  else if(button.innerHTML =="Pause") {
-    audio.pause();
-    button.innerHTML = "Play";
-  }
-  else {
-
-  }
-
-
-
-}
-
-function initPlayerTest() {
-  DZ.init({
-  appId  : '	540582',
-  channelUrl : 'http://localhost:80/SongPreviews/index.php',
-  player : {
-    onload : function(){
-    }
-  }
-});
-}
->>>>>>> 7a5206fab97d79f851cd14ce8971c78ea8d369f9
